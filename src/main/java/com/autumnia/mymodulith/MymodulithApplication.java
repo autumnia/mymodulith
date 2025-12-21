dea7cdfdb678a7665f83eb7612ebabd617432cdb
김가을 <autumnya@gmail.com>
2025-12-21 21:58:07 +0900

    firt commit

---
+++ src/main/java/com/autumnia/mymodulith/MymodulithApplication.java
@@ -0,0 +1,15 @@
+package com.autumnia.mymodulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@ImportRuntimeHints(ModulithRegistrar.class)
+@SpringBootApplication
+public class MymodulithApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MymodulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autumnia/mymodulith/MymodulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autumnia.mymodulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymodulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
